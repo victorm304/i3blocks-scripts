fd4b244ccfc9e9ea8d22876b869b3c01169a62bc
victorm304 <143431343+victorm304@users.noreply.github.com>
2024-05-29 21:53:35 -0300

    Update dolar.py

--- dolar.py
+++ dolar.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from httpx import get
 
-url = get('https://api.hgbrasil.com/finance')
+url = get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 dolar_icon ='💲'
 
 if url.status_code == 200:
     dados = url.json()
-    dolar_brl = dados.get('results').get('currencies').get('USD').get('buy')
-    print(f'{dolar_icon} {round(dolar_brl,2)} BRL')
+    dolar_brl = dados['USDBRL']['bid']
+    print(f'{dolar_icon} {dolar_brl[0:4]} BRL')
